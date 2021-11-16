a4cf43147ad34e43c9f8e55aea3574608a017546
Thomas Goodwin <thomas@goodiemania.org>
2021-11-17 09:43:44 +1300

    BSing code to read the JSON response

--- src/main/java/org/goodiemania/j4nzcp/impl/SignatureValidator.java
+++ src/main/java/org/goodiemania/j4nzcp/impl/SignatureValidator.java
@@ -1,14 +1,26 @@
 package org.goodiemania.j4nzcp.impl;
 
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.GetRequest;
 import org.goodiemania.j4nzcp.impl.entities.NewZealandCovidPass;
+import org.json.JSONObject;
 
 public class SignatureValidator {
     public void validate(final NewZealandCovidPass covidPass) {
         String substring = "http://" + covidPass.payload().iss().substring(8) + "/.well-known/did.json";
         GetRequest getRequest = Unirest.get(substring);
-        System.out.println(substring);
+        try {
+            Unirest.get(substring).asJson().getBody().getObject()
+                    .getJSONArray("verificationMethod")
+                    .getJSONObject(0)
+                    .getJSONObject("publicKeyJwk")
+                    .get("kty");
+
+            System.out.println(substring);
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
 
 
         //TODO next we need to process the pass.payload().iss() and turn it into https://nzcp.covid19.health.nz/.well-known/did.json
